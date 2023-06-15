28a200b1a878290beb12962f643ec34ffc7f4c16
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-06-16 00:40:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ function Person(name, age) {
 	this.age = age;
 }
 Person.prototype.greet = function(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-}
-
+	console.log("Hello, my name is "+this.name+" and I am "+this.age+" years old.");
+}	
 function Employee(name, age, jobTitle) {
-	Person.call(this,name,age)
+	Person.call(this,name,age);
 	this.jobTitle = jobTitle;
 }
 Employee.prototype = Object.create(Person.prototype);
